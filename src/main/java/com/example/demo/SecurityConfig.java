025a32d30371ed14fe9a25e588751487b639fd0b
Cosme Torandell Borrás <91748301+cosmetorandellborras@users.noreply.github.com>
2023-02-16 20:12:20 +0100

    ctorandellborras@cifpfbmoll.es ws exam US3

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -19,3 +19,11 @@ public class SecurityConfig {
     }
 
 }
+/*S' ha creat un certificat SSL que s'empra per validar la conexio entrant
+
+Per un altre lloc, es crea un fitxer application.yml que conte el port per el cual es processaran les peticions https,
+aquest fitxer conte la configuració que s empra per tal d acceptar les peticions HTTPS a mes de la referencia al fitxer SSL creat, del cual fa us
+
+Per finalitzar, es crea la clase SecurityConfig per recopilar les politiques de seguretat y configurar un canal per a que la aplicacio sigui
+segura per a totes les solicituds.
+ */
